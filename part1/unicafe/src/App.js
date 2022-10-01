cf7ee600a7f3cad221f7b411f47093f9af38edde
nguyenfamj <nguyenfamj1409@gmail.com>
2022-10-02 01:04:11 +0300

    Done exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -35,12 +35,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <div>
         <h2>Statistics</h2>
-        <StatisticLine text='Good' value={good} />
-        <StatisticLine text='Neutral' value={neutral} />
-        <StatisticLine text='Bad' value={bad} />
-        <StatisticLine text='All' value={all} />
-        <StatisticLine text='Average' value={all / 3} />
-        <StatisticLine text='Positive' value={`${(good / all) * 100}%`} />
+        <table>
+          <tbody>
+            <StatisticLine text='Good' value={good} />
+            <StatisticLine text='Neutral' value={neutral} />
+            <StatisticLine text='Bad' value={bad} />
+            <StatisticLine text='All' value={all} />
+            <StatisticLine text='Average' value={all / 3} />
+            <StatisticLine text='Positive' value={`${(good / all) * 100}%`} />
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -58,9 +62,10 @@ const Button = ({ text, onClick }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
