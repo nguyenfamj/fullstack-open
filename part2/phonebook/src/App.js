fba0074ede274ca43de8ab453f7ed80f084655c5
nguyenfamj <nguyenfamj1409@gmail.com>
2022-12-21 23:19:29 +0100

    Done exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', phone: '0321234584' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-  console.log(newNumber);
+  console.log(searchTerm);
 
   const onInputNameChange = (event) => {
     setNewName(event.target.value);
@@ -13,6 +19,10 @@ const App = () => {
 
   const onInputNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const onSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
   };
 
   const onAddNewName = (event) => {
@@ -24,13 +34,19 @@ const App = () => {
     setPersons(persons.concat({ name: newName, phone: newNumber }));
     setNewName('');
     setNewNumber('');
-
-    console.log(persons);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div style={{ marginBottom: 20 }}>
+        <input
+          type='text'
+          value={searchTerm}
+          onChange={onSearchTermChange}
+          placeholder='Search people'
+        />
+      </div>
       <form>
         <div>
           name: <input value={newName} onChange={onInputNameChange} />
@@ -45,12 +61,16 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name} style={{ display: 'flex' }}>
-          <div>{person.name}</div>
-          <div style={{ marginLeft: 10 }}>{person.phone}</div>
-        </div>
-      ))}
+      {persons
+        .filter((person) => {
+          return person.name.toLowerCase().includes(searchTerm.toLowerCase());
+        })
+        .map((person, index) => (
+          <div key={index} style={{ display: 'flex' }}>
+            <div>{person.name}</div>
+            <div style={{ marginLeft: 10 }}>{person.phone}</div>
+          </div>
+        ))}
     </div>
   );
 };
